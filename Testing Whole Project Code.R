67897149b020537bc23a5ee7e1045eba8137d333
poolt <poolt@seattleu.edu>
2019-12-13 16:00:13 -0800

    Again, trying to save script and data with script inside of the project

---
+++ Testing Whole Project Code.R
@@ -0,0 +1,20 @@
+
+
+
+
+
+New Project to see if it saves together
+
+############################################
+############################################
+############################################
+
+###    Org Data    #####
+library(cars)
+load(cars)
+Heerhartz_Basic<-as.data.frame(Heerhartz_Seahurst_CSV)
+
+###    Analyses    #####
+
+
+###    Vis Data    #####
